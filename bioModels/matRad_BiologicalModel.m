76c68c0e9455c312c5dd515d4039b3703bdb0071
remocristoforetti <recristoforetti@gmail.com>
2023-05-30 17:57:54 +0200

    changed load of machine data & name of property

--- bioModels/matRad_BiologicalModel.m
+++ bioModels/matRad_BiologicalModel.m
@@ -4,7 +4,7 @@ classdef matRad_BiologicalModel < handle
         model;
         radiationMode;
         
-        baseData;
+        machine;
         calcBioParameters = 0;
     end
 
@@ -30,7 +30,7 @@ classdef matRad_BiologicalModel < handle
                     if any(strcmp(value,obj.AvailableRadiationModalities))
                         matRad_cfg.dispInfo('Biological model successfuly instantiated.\n');
                         obj.radiationMode = value;
-                        obj.baseData = [];
+                        obj.machine = [];
                         obj.updateStatus();
                     else
                         matRad_cfg.dispError(['Selected radiation modality is not available for model: ', obj.model, '\n']);
@@ -44,72 +44,67 @@ classdef matRad_BiologicalModel < handle
         % base Data
         
 
-        function set.baseData(obj,value)
+        function set.machine(obj,value)
 
             matRad_cfg = MatRad_Config.instance();
 
-            currBaseData = obj.baseData;
-            if ~isempty(obj.radiationMode)
-                 currmachine_name = [obj.radiationMode, '_', currBaseData];
-                 machine_name     = [obj.radiationMode, '_', value];
+            currmachine = obj.machine;
+            if  ~isempty(value)
+                machine_name = [value.meta.machine];
+            else
+                machine_name = [];
             end
-            if ~strcmp(currmachine_name,machine_name) && ~(isempty(currBaseData) && isempty(value))
+            
+            if  ~isempty(currmachine)
+                currmachine_name = [currmachine.meta.machine];
+            else
+                currmachine_name = [];
+            end
+
+            if ~isempty(obj.radiationMode)
+                 currmachine_name = [obj.radiationMode, '_', currmachine_name];
+                 machine_name     = [obj.radiationMode, '_', machine_name];
+            end
+            if ~(isempty(currmachine) && isempty(value)) && ~strcmp(currmachine_name,machine_name)
                 if ~isempty(value)
                     if ~isempty(obj.radiationMode) %here if radiationMode is empty, dose nothing -> change
                                 
                            matRad_cfg.dispInfo('Checking validity of base data ...');
-                            
-                           try
-                              machine_name = [obj.radiationMode, '_', value];
-                              machine_data = load([matRad_cfg.matRadRoot filesep 'basedata' filesep machine_name '.mat']);
-                              obj.baseData = value;
-                           catch %If does not work, try generic
-                              matRad_cfg.dispWarning('Could not find any valid baseData file: %s. Trying with Generic.\n', value);
-                              try
-                                  machine_name = [obj.radiationMode, '_', 'Generic'];
-                                  machine_data = load([matRad_cfg.matRadRoot filesep 'basedata' filesep machine_name '.mat']);
-                                  obj.baseData = 'Generic';
-                              catch %Give up
-                                  matRad_cfg.dispWarning('Could not find any Generic machine file.\n');
-                                  obj.baseData = [];
-                              end
-                           end
+                           
+                           fieldNames = fieldnames(value.data(1));
+                           if ~isempty(obj.RequiredBaseData) && ~isempty(fieldNames)
             
-                           %If loaded some machine files, see if has all the required
-                           %data
-            
-                           if ~isempty(obj.RequiredBaseData) && ~isempty(obj.baseData)
-            
-                               ValidbaseData = 0;
+                               Validmachine = 0;
                                for k =1:size(obj.RequiredBaseData,2)
                                       
-                                   if ~(isfield(machine_data.machine.data, obj.RequiredBaseData{k}))
+                                   if ~any(strcmp(fieldNames, obj.RequiredBaseData{k}))
                                       matRad_cfg.dispWarning('Could not find the following machine data: %s.\n',obj.RequiredBaseData{k});
-                                      ValidbaseData = 0;
+                                      Validmachine = 0;
                                    else
-                                      ValidbaseData =  ValidbaseData + 1;
+                                      Validmachine =  Validmachine + 1;
                                    end
                                end
            
-                               if ValidbaseData ~= size(obj.RequiredBaseData,2)            
+                               if Validmachine ~= size(obj.RequiredBaseData,2)            
                                    matRad_cfg.dispWarning('Insufficient base data provided for this configuration. Biological optimization turned down\n');
-                                   obj.baseData = [];
+                                   obj.machine = [];
             
                                else
                                    matRad_cfg.dispInfo(' done\n');
+                                   obj.machine = value;
                                    obj.updateStatus();
                                end
             
                            elseif isempty(obj.RequiredBaseData)
                                 matRad_cfg.dispInfo(' done\n');
-                                matRad_cfg.dispInfo(['No baseData required for model: ', obj.model, '\n']);
-                                obj.baseData = [];
+                                matRad_cfg.dispInfo(['No machine required for model: ', obj.model, '\n']);
+                                obj.machine = [];
                                 obj.updateStatus();
                            end
                     end
                  else % if isempty(value)
-                        %The input value is empty, just reset the baseData to empty
-                       obj.baseData = [];
+                        %The input value is empty, just reset the machine to empty
+                       obj.machine = [];
                        obj.updateStatus();
                 end % end ~isempty(value)
             end
@@ -124,20 +119,20 @@ classdef matRad_BiologicalModel < handle
             %currRadiationMode = obj.radiationMode;
             %obj.radiationMode = currRadiationMode;
 
-            currBaseData = obj.baseData;
-            obj.baseData = currBaseData;
+            currmachine = obj.machine;
+            obj.machine = currmachine;
 
 
             currRadiationMode = obj.radiationMode;
-            currBaseData = obj.baseData;
+            currmachine = obj.machine;
 
-            if ~isempty(currRadiationMode) && ~isempty(currBaseData)
+            if ~isempty(currRadiationMode) && ~isempty(currmachine)
                 %For now, exclude alpha/beta calculation with photons
                 if ~strcmp(obj.radiationMode, 'photons')
                     obj.calcBioParameters = 1;
                     matRad_cfg.dispInfo('All parameters are correct, enabling biological model calculation.\n');
                 end
-            % elseif ~isempty(currRadiationMode) && isempty(currBaseData) && isempty(obj.RequiredBaseData)
+            % elseif ~isempty(currRadiationMode) && isempty(currmachine) && isempty(obj.RequiredBaseData)
             %      obj.calcBioParameters = 1;
             %      matRad_cfg.dispInfo('All parameters are correct, enabling biological model calculation.\n');
             else
@@ -148,4 +143,5 @@ classdef matRad_BiologicalModel < handle
 
 
     end
+
 end
