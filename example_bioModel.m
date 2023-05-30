30389b54b3531803b1289c87ef30eba37bdf097c
remocristoforetti <recristoforetti@gmail.com>
2023-05-30 16:25:48 +0200

    example test for biologcialModels

--- example_bioModel.m
+++ example_bioModel.m
@@ -1,7 +1,9 @@
 %load generic matRad environment and phantom
+clear all;
 matRad_rc;
 matRad_cfg = MatRad_Config.instance();
 load('BOXPHANTOM.mat');
+
 %% Setup
 
 %Define generic plan info
@@ -21,27 +23,42 @@ pln.propDoseCalc.doseGrid.resolution.y = 8; % [mm]
 pln.propDoseCalc.doseGrid.resolution.z = 8; % [mm]
 
 %% Select radiation mode and machine
-pln.radiationMode   = 'carbon'; % 'carbon', 'helium'
+pln.radiationMode   = 'protons'; % 'carbon', 'helium'
 
 pln.machine         = 'Generic';
 
 %% Instantiate the specific Biological model for calculation
-modelName = 'LEM';
+modelName = 'constRBE';
 pln.bioParam = matRad_bioModel(pln.radiationMode,modelName);
 %% Generate the stf file
 stf = matRad_generateStf(ct,cst,pln);
 
 %% Select generic plan info
 pln.propDoseCalc.calcLET = 1;
-quantityOpt = 'RBExD';
-
-%% Compute example of LET based bioModel
-%Set the biological parameters;
-% modelName = 'MCN';
-% pln.bioParam = matRad_bioModel(pln.radiationMode,quantityOpt,modelName,pln.machine);
 
 %% Compute the dij for the MCN model
-dij_MCN = matRad_calcParticleDose(ct,stf,pln,cst,0);
+dij = matRad_calcParticleDose(ct,stf,pln,cst,0);
+%dij = matRad_calcPhotonDose(ct,stf,pln,cst,0);
+%% Select optimization quantity
+pln.propOpt.quantityOpt = 'physicalDose';
 
 %% Optimize the SOBP
-resultGUI_MCN = matRad_fluenceOptimization(dij_MCN,cst,pln);
+resultGUI_MCN = matRad_fluenceOptimization(dij,cst,pln);
+
+%% Other models
+modelNames = {'WED', 'CAR'};
+
+resultGUI_LETmodel = [];
+for k=1:size(modelNames,2)
+    pln.bioParam = matRad_bioModel(pln.radiationMode,modelNames{k});
+    dij = matRad_calcParticleDose(ct,stf,pln,cst,0);
+    resultGUI_LETmodel = [resultGUI_LETmodel,matRad_fluenceOptimization(dij,cst,pln)];
+end
+
+%% constRBE
+modelName = 'constRBE';
+pln.bioParam = matRad_bioModel(pln.radiationMode,modelName);
+
+dij_constRBE = matRad_calcParticleDose(ct,stf,pln,cst,0);
+
+resultGUI_constRBE = matRad_fluenceOptimization(dij_constRBE,cst,pln);
