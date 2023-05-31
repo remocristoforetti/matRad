2666914ae168b641b3c59ebe399e8e5334be6272
remocristoforetti <recristoforetti@gmail.com>
2023-05-31 14:28:08 +0200

    setup of HEL model, not tested!

--- bioModels/matRad_bioModel_HEL.m
+++ bioModels/matRad_bioModel_HEL.m
@@ -1,4 +1,4 @@
-classdef matRad_bioModel_HEL < matRad_Bio_Model
+classdef matRad_bioModel_HEL < matRad_BiologicalModel
       properties (SetAccess = private)%constant
         
         p1_HEL = NaN;
@@ -9,8 +9,6 @@ classdef matRad_bioModel_HEL < matRad_Bio_Model
 
         AvailableRadiationModalities = {'helium'};
         RequiredBaseData = {'depths','offset','LET'};
-        requiredDijFields = {'mAlphaDose', 'mSqrtBetaDose'};
-
       end
 
 
@@ -22,12 +20,11 @@ classdef matRad_bioModel_HEL < matRad_Bio_Model
            matRad_dispToConsole(['Something wrong with bioModel inputs'],[],'warning');
           end
           
-          obj@matRad_Bio_Model();
-%          obj.model = obj.Model_name;
+          obj@matRad_BiologicalModel(radiationMode);
           obj.model = 'HEL';
        end
        
-       function str = calcTissueParameters(obj,cst,numVoxels,ctScen)
+       function str = calcTissueParameters(obj,cst,numVoxels,stf,ctScen)
            matRad_cfg =  MatRad_Config.instance();
 
            str = struct('alphaX', [], ...
@@ -37,39 +34,25 @@ classdef matRad_bioModel_HEL < matRad_Bio_Model
            
            TissueParam = {cst{:,5}};
            tissueClass = zeros(numVoxels,1);
-           
-           try
-              load([matRad_cfg.matRadRoot filesep 'basedata' filesep strcat(obj.radiationMode, '_', obj.baseData) '.mat']);
-           catch
-              matRad_cfg.dispError(['Could not find the following machine file: ' obj.machine '\n']);
-           end
 
-           if any(machine)
+           machine = obj.machine;
+
+           if exist('machine', 'var')
               for k = 1:size(TissueParam,2)
-
-                 if ~isfield(TissueParam{k}, 'alphaX') || (isfield(TissueParam{k}, 'alphaX') && ~any(ismember(machine.data(1).alphaX,TissueParam{k}.alphaX)))
-                     matRad_cfg.dispWarning(['Warning! Tissue parameter AlphaX for tissue ' cst{k,1} 'not available, set do default \n']);
-                     if ismember(obj.default_AlphaX, machine.data(1).alphaX)
-                         str.alphaX = [str.alphaX, obj.default_AlphaX];
-                     else
-                         str.alphaX = [str.alphaX, machine.data(1).alphaX(1)];
-                         matRad_cfg.dispWarning(['Warning! Tissue parameter default_AlphaX for tissue ' cst{k,1} 'not compatible with basedata, setting the available one \n']);
-                     end
-                 else
-                     str.alphaX = [str.alphaX, TissueParam{k}.alphaX];
-                 end
-
-                 if ~isfield(TissueParam{k}, 'betaX') || (isfield(TissueParam{k}, 'beatX') && ~any(ismember(machine.data(1).betaX,TissueParam{k}.betaX)))
-                     matRad_cfg.dispWarning(['Warning! Tissue parameter BetaX for tissue ' cst{k,1} 'not available, set do default \n']);
-                     if ismember(obj.default_BetaX, machine.data(1).betaX)
-                         str.betaX = [str.betaX, obj.default_BetaX];
-                     else
-                         str.betaX = [str.betaX, machine.data(1).betaX(1)];
-                         matRad_cfg.dispWarning(['Warning! Tissue parameter default_BetaX for tissue ' cst{k,1} 'not compatible with basedata, setting the available one \n']);
-                     end    
-                 else
-                     str.betaX = [str.betaX, TissueParam{k}.betaX];
-                 end
+    
+              if ~isfield(TissueParam{k}, 'alphaX')
+                  str.alphaX = [str.alphaX, obj.default_AlphaX];
+                  matRad_cfg.dispWarning(['Warning! Tissue parameter AlphaX for tissue %i not available, set do default'], cst{k,1});
+              else
+                  str.alphaX = [str.alphaX, TissueParam{k}.alphaX];
+              end
+               
+              if ~isfield(TissueParam{k}, 'betaX')
+                  str.betaX = [str.betaX, obj.default_BetaX];
+                  matRad_cfg.dispWarning(['Warning! Tissue parameter BetaX for tissue %i not available, set do default '],  cst{k,1});
+              else
+                  str.betaX = [str.betaX, TissueParam{k}.betaX];
+              end
                  
                  str.tissueABratio(k) = str.alphaX(k)./str.betaX(k);
                  tissueClass(cst{k,4}{1},1) = k;
@@ -79,9 +62,10 @@ classdef matRad_bioModel_HEL < matRad_Bio_Model
        end
 
        function [bixelAlpha,bixelBeta] = calcLQParameter(obj,vRadDepths,baseDataEntry,TissueParam,ix);
-            
-            bixelAlpha = NaN*ones(numel(vRadDepths),1);
-            bixelBeta  = NaN*ones(numel(vRadDepths),1);
+            % This model has not been tested!
+   
+           bixelAlpha = NaN*ones(numel(vRadDepths),1);
+           bixelBeta  = NaN*ones(numel(vRadDepths),1);
             
             % range shift
             depths = baseDataEntry.depths + baseDataEntry.offset;
@@ -101,13 +85,13 @@ classdef matRad_bioModel_HEL < matRad_Bio_Model
                   % linear exponential fit
                   %f_LE      = (2.965e-1 * bixelLET) .* exp(-4.90821e-3 * bixelLET);
                   %RBEmax_LE = 1 + ((1.5384e-1  + (vABratio.^-1)) .* f_LE);
-                  vAlpha_x = TissueParam.alphaX(TissueParameter.tissueClass(ix));
-                  vBeta_x = TissueParam.betaX(TissueParameter.tissueClass(ix));
-                  vABRatio = TissueParam.tissueABratio(TisseuParameter.tissueClass(ix));
+                  vAlpha_x = TissueParam.alphaX(TissueParam.tissueClass(ix))';
+                  vBeta_x = TissueParam.betaX(TissueParam.tissueClass(ix))';
+                  vABRatio = TissueParam.tissueABratio(TissueParam.tissueClass(ix))';
                   
                   % quadratic exponential fit
                   f_QE      = (obj.p1_HEL * bixelLET.^2) .* exp(-obj.p2_HEL * bixelLET);
-                  RBEmax_QE = 1 + ((obj.p0_HEL  + (vABratio.^-1)) .* f_QE);
+                  RBEmax_QE = 1 + ((obj.p0_HEL  + (vABRatio.^-1)) .* f_QE);
 
                   % the linear quadratic fit yielded the best fitting result
                   RBEmax = RBEmax_QE;
