02d979644c377b6f8bad567e68381422138b3fd9
RemoCristoforetti <recristoforetti@gmail.com>
2024-04-30 10:59:39 +0200

    Update stf grid for multiple angles and speciific spot positioning

--- matRad_generateStfSpotGridForTesting.m
+++ matRad_generateStfSpotGridForTesting.m
@@ -5,28 +5,31 @@ function stf = matRad_generateStfSpotGridForTesting(ct,cst,pln,energies,x,y)
     %     spotGridSize = [1 1];
     % end
 
-    if ~exist('x', 'var') || isempty(x)
-        x = zeros(spotGridSize(1)*spotGridSize(2),numel(energies));
-    end
+    % if ~exist('x', 'var') || isempty(x)
+    %     x = zeros(spotGridSize(1)*spotGridSize(2),numel(energies));
+    % end
+    % 
+    % if ~exist('y', 'var') || isempty(y)
+    %     y = zeros(spotGridSize(1)*spotGridSize(2), numel(energies));
+    % end
+    % 
+    % if ~exist('div', 'var') || isempty(div)
+    %     div = 1;
+    % end
+
+    %[xGrid, yGrid] = meshgrid(x,y);
     
-    if ~exist('y', 'var') || isempty(y)
-        y = zeros(spotGridSize(1)*spotGridSize(2), numel(energies));
-    end
+    % xGrid_V = xGrid(:);
+    % yGrid_V = yGrid(:);
+    xGrid_V = x;
+    yGrid_V = y;
     
-    if ~exist('div', 'var') || isempty(div)
-        div = 1;
-    end
-
-    [xGrid, yGrid] = meshgrid(x,y);
-
-    xGrid_V = xGrid(:);
-    yGrid_V = yGrid(:);
-
     nEnergies = numel(energies);
     nSpots = numel(xGrid_V);
 
     stf = matRad_generateStf(ct, cst,pln);
-    stf.ray = struct();
+    for i=1:numel(stf)
+        stf(i).ray = struct();
     % stf.ray = struct('rayPos_bev', [],...
     %     'targetPoint_bev', [],...
     %     'rayPos', [],...
@@ -34,28 +37,34 @@ function stf = matRad_generateStfSpotGridForTesting(ct,cst,pln,energies,x,y)
     %     'energy', [],...
     %     'rangeShifter', [],...
     %     'focusIx', []);
+        currPln = pln;
+        currPln.propStf.gantryAngles = pln.propStf.gantryAngles(i);
+        currPln.propStf.couchAngles  = pln.propStf.couchAngles(i);
+        currPln.propStf.isoCenter    = pln.propStf.isoCenter(i,:);
+
+        for spotIdx=1:nSpots
     
-    for spotIdx=1:nSpots
+                    tmpStf = matRad_generateStfSinglePencilBeam(ct, cst,currPln,energies(1),xGrid_V(spotIdx), yGrid_V(spotIdx));
+    
+                    tmpRay = tmpStf.ray;
+    
+                    tmpFields = fieldnames(tmpRay);
+                    for k=1:numel(tmpFields)
+                        stf(i).ray(spotIdx).(tmpFields{k}) = tmpRay.(tmpFields{k});
+                    end
+                    stf(i).ray(spotIdx).energy = energies;
+                    stf(i).ray(spotIdx).rangeShifter = repmat(tmpRay.rangeShifter, 1, nEnergies);
+                    stf(i).ray(spotIdx).focusIx = repmat(tmpRay.focusIx, 1,nEnergies);
+                    stf(i).ray(spotIdx).numParticlesPerMU = repmat(10^6, 1, nEnergies);
+                    stf(i).ray(spotIdx).minMU = zeros(1,nEnergies);
+                    stf(i).ray(spotIdx).maxMU = Inf(1,nEnergies);         
+        end
 
-                tmpStf = matRad_generateStfSinglePencilBeam(ct, cst,pln,energies(1),xGrid_V(spotIdx), yGrid_V(spotIdx));
 
-                tmpRay = tmpStf.ray;
-
-                tmpFields = fieldnames(tmpRay);
-                for k=1:numel(tmpFields)
-                    stf.ray(spotIdx).(tmpFields{k}) = tmpRay.(tmpFields{k});
-                end
-                stf.ray(spotIdx).energy = energies;
-                stf.ray(spotIdx).rangeShifter = repmat(tmpRay.rangeShifter, 1, nEnergies);
-                stf.ray(spotIdx).focusIx = repmat(tmpRay.focusIx, 1,nEnergies);
-                stf.ray(spotIdx).numParticlesPerMU = repmat(10^6, 1, nEnergies);
-                stf.ray(spotIdx).minMU = zeros(1,nEnergies);
-                stf.ray(spotIdx).maxMU = Inf(1,nEnergies);         
+        stf(i).numOfBixelsPerRay = nEnergies*ones(1,nSpots);
+        
+        stf(i).totalNumOfBixels = nEnergies*nSpots;
+        stf(i).numOfRays       = nSpots;
     end
 
-
-    stf.numOfBixelsPerRay = nEnergies*ones(1,nSpots);
-    
-    stf.totalNumOfBixels = nEnergies*nSpots;
-    stf.numOfRays       = nSpots;
 end
