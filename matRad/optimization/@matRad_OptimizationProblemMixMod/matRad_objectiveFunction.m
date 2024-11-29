6f61af9ea8d41a1d078f700c2564a2e0d261056e
RemoCristoforetti <recristoforetti@gmail.com>
2024-11-29 09:28:15 +0100

    Add effect fraction adaptation in optiprob

--- matRad/optimization/@matRad_OptimizationProblemMixMod/matRad_objectiveFunction.m
+++ matRad/optimization/@matRad_OptimizationProblemMixMod/matRad_objectiveFunction.m
@@ -63,13 +63,31 @@ for  i = 1:size(cst,1)
         % loop over the number of constraints for the current VOI
         for j = 1:numel(cst{i,6})
             
+
             objective = cst{i,6}{j};
             
             % only perform gradient computations for objectiveectives
             if isa(objective,'DoseObjectives.matRad_DoseObjective')
                 
-                % rescale dose parameters to biological optimization quantity if required
+                 % Obtain the fraction-size dose prescription to be
+                % converted to biological prescription. When performing
+                % effect-based optimization, this step ensures that the
+                % dose-effect conversion is always performed on a
+                % fraction-size dose prescription and only after scaled back to
+                % total plan effect.
+
+                % First scale the dose prescription to fraction size
+                doseParameter = objective.getDoseParameters();
+                objective = objective.setDoseParameters(doseParameter./optiProb.BP.totalNumOfFractions);
+
+                % Compute the biological parameters. This function is only effective when using effect-based
+                % optimization
                 objective = optiProb.BP.setBiologicalDosePrescriptions(objective,cst{i,5}.alphaX,cst{i,5}.betaX);
+           
+                % Scale back the biological dose prescription to total plan
+                % size
+                doseParameter = objective.getDoseParameters();
+                objective = objective.setDoseParameters(doseParameter.*optiProb.BP.totalNumOfFractions);
 
                 % retrieve the robustness type
                 robustness = objective.robustness;
--- matRad/optimization/@matRad_OptimizationProblemMixMod/matRad_objectiveGradient.m
+++ matRad/optimization/@matRad_OptimizationProblemMixMod/matRad_objectiveGradient.m
@@ -63,6 +63,7 @@ vOmega = 0;
 f_COWC = zeros(size(dij.(dij.radiationModalities{1}).physicalDose));
 
 % compute objective function for every VOI.
+
 for  i = 1:size(cst,1)
    
     % Only take OAR or target VOI.
@@ -80,9 +81,27 @@ for  i = 1:size(cst,1)
                 % retrieve the robustness type
                 robustness = objective.robustness;
                 
-                % rescale dose parameters to biological optimization quantity if required
+                % Obtain the fraction-size dose prescription to be
+                % converted to biological prescription. When performing
+                % effect-based optimization, this step ensures that the
+                % dose-effect conversion is always performed on a
+                % fraction-size dose prescription and only after scaled back to
+                % total plan effect.
+    
+                % First scale the dose prescription to fraction size
+                doseParameter = objective.getDoseParameters();
+                objective = objective.setDoseParameters(doseParameter./optiProb.BP.totalNumOfFractions);
+    
+                % Compute the biological parameters. This function is only effective when using effect-based
+                % optimization
+    
                 objective = optiProb.BP.setBiologicalDosePrescriptions(objective,cst{i,5}.alphaX,cst{i,5}.betaX);
-                
+           
+                % Scale back the biological dose prescription to total plan
+                % size
+                doseParameter = objective.getDoseParameters();
+                objective = objective.setDoseParameters(doseParameter.*optiProb.BP.totalNumOfFractions); 
+
                 switch robustness
                     case 'none' % if conventional opt: just sum objectiveectives of nominal dose
                         for s = useNominalCtScen
