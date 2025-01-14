ac10427228d46af5c58d2b9b45ab263e8d042bec
RemoCristoforetti <recristoforetti@gmail.com>
2025-01-14 13:07:04 +0100

    Update scenario dumping functions

--- matRad/scenariosDumping/matRad_loadScenariosFromMeta.m
+++ matRad/scenariosDumping/matRad_loadScenariosFromMeta.m
@@ -1,12 +1,12 @@
-function physicalDose = matRad_loadScenariosFromMeta(saveDir, scenariosMeta, dijTemplate)
+function [physicalDose, physicalDoseOmegaReduced, mAlphaDose, mSqrtBetaDose, mLETDose,mLETdJ, alphaJ, sqrtBetaJ,physicalDoseJ]  = matRad_loadScenariosFromMeta(saveDir, scenariosMeta, dijTemplate, verbosity)
 
     matRad_cfg = MatRad_Config.instance();
-
+    %% Input
     if ~exist('dijTemplate', 'var') || isempty(dijTemplate)
 
         matRad_cfg.dispInfo('No dijTemplate provided, trying to locate one ...');
         try
-            load(fullfile(saveDirectory, 'dijTemplate'));
+            load(fullfile(saveDirectory, 'dijTemplate.mat'),'dijTemplate');
             matRad_cfg.dispInfo('done. \n');
         catch
 
@@ -14,22 +14,92 @@ function physicalDose = matRad_loadScenariosFromMeta(saveDir, scenariosMeta, dij
         end
     end
 
+    if ~exist('verbosity', 'var') || isempty(verbosity)
+
+        verbosity = false;
+    end
+
+    %% Output
+    %nOutputs = nargout();
+    %TODO
+    %% Loading
+    physicalDose             = {};
+    physicalDoseOmegaReduced = {};
+    mAlphaDose               = {};
+    mSqrtBetaDose            = {};
+    mLETDose                 = {};
+    mLETdJ                   = {};
+    alphaJ                   = {};
+    sqrtBetaJ                = {};
+    physicalDoseJ            = {};
+
     % Get meta info from dij template
     nVoxels = dijTemplate.doseGrid.numOfVoxels;
     nBixels = dijTemplate.totalNumOfBixels;
 
     nScensToLoad = numel(scenariosMeta);
     
-    
     physicalDose = arrayfun(@(scen) spalloc(nVoxels,nBixels,scen.nnzElements), scenariosMeta, 'UniformOutput',false);
+    %mAlphaDose = arrayfun(@(scen) spalloc(nVoxels,nBixels,scen.nnzElements), scenariosMeta, 'UniformOutput',false);
+    %mSqrtBetaDose = arrayfun(@(scen) spalloc(nVoxels,nBixels,scen.nnzElements), scenariosMeta, 'UniformOutput',false);
+
+    alphaJ        = cell(nScensToLoad,1);
+    sqrtBetaJ     = cell(nScensToLoad,1);
+    % sqrtBetaJ     = arrayfun(@(scen) spalloc(nBixels,1, nBixels), scenariosMeta, 'UniformOutput',false);
     
+    
+    stringLength = 0;
     for scenIdx=1:nScensToLoad
 
+        if verbosity
+            fprintf(repmat('\b',1,stringLength));
+            stringLength = fprintf('\tLoading scenario: %u/%u\n', scenIdx, nScensToLoad);
+        end
+        
         currScenarioMeta = scenariosMeta(scenIdx);
         fileName = fullfile(saveDir, currScenarioMeta.name);
         
         currDijScen = load(fileName, 'dijScenario');
         currDijScen = currDijScen.dijScenario;
-        physicalDose(scenIdx) = currDijScen;
+
+        if ~isstruct(currDijScen)
+            % This is for older compatibility
+            physicalDose(scenIdx) = currDijScen;
+        else
+            physicalDose(scenIdx)  = currDijScen.physicalDose;
+            
+            if isfield(currDijScen, 'mAlphaDose')
+                mAlphaDose{scenIdx}   = currDijScen.mAlphaDose;
+            end
+
+            if isfield(currDijScen, 'mSqrtBetaDose')
+                mSqrtBetaDose{scenIdx} = currDijScen.mSqrtBetaDose;
+            end
+
+            if isfield(currDijScen, 'mLETDose')
+                mLETDose{scenIdx} = currDijScen.mLETDose;
+            end
+
+            if isfield(currDijScen, 'alphaJ') && isfield(currDijScen, 'sqrtBetaJ')
+                alphaJ{scenIdx}    = currDijScen.alphaJ;
+                sqrtBetaJ{scenIdx} = currDijScen.sqrtBetaJ;
+            end
+            
+            if isfield(currDijScen, 'physicalDoseOmegaReduced')
+                physicalDoseOmegaReduced{scenIdx} = currDijScen.physicalDoseOmegaReduced;
+            else
+                physicalDoseOmegaReduced{scenIdx} = [];
+            end
+
+            if isfield(currDijScen, 'mLETdJ')
+                mLETdJ{scenIdx} = currDijScen.mLETdJ;
+            end
+
+            
+            if isfield(currDijScen, 'mLETdJ')
+                physicalDoseJ{scenIdx} = currDijScen.physicalDoseJ;
+            end
+
+        end
     end
 end
